974ff875c0bc47b0dfe2b2d1ce202629e5a992b2
Sacha Laville <sacha_laville@outlook.fr>
2026-02-19 21:14:17 +0100

    update: ajout de style a la TabBar

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -3,24 +3,29 @@ import {StyleSheet, Text, View, Image, Button} from 'react-native';
 import { FontAwesome } from '@react-native-vector-icons/fontawesome6';
 import HomeScreen from '../screens/HomeScreen';
 import SportScreen from '../screens/SportScreen';
-import NutritionScreen from '../screens/NutritionScreen'
-import TrackScreen from '../screens/TrackScreen'
+import NutritionScreen from '../screens/NutritionScreen';
+import TrackScreen from '../screens/TrackScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import iconSet from '@expo/vector-icons/build/Fontisto';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Tab = createBottomTabNavigator();
 
 function NavBar() {
   return (
     <Tab.Navigator screenOptions={{headerShown:false, tabBarStyle:{backgroundColor:'#141319', borderColor:'#505050', }}}>
-      <Tab.Screen name="Accueil" component={HomeScreen} />
-      <Tab.Screen name="Sport" component={SportScreen} />
-      <Tab.Screen name="Nutrition" component={NutritionScreen} />
-      <Tab.Screen name="Track" component={TrackScreen} />
+      <Tab.Screen name="Accueil" component={HomeScreen} 
+      options={{tabBarIcon:() => <FontAwesomeIcon icon="fa-solid fa-house" style={{color: "rgb(255, 255, 255)",}} /> }}/>
+      <Tab.Screen name="Sport" component={SportScreen}
+      options={{tabBarIcon:() => <MaterialIcons name="sports-handball" size={24} color="white" /> }}/>
+      <Tab.Screen name="Nutrition" component={NutritionScreen}       
+      options={{tabBarIcon:() => <Image source={require('../src/images/HomePage.png')} style={{width:24, height:24}} /> }}/>
+      <Tab.Screen name="Traqueur" component={TrackScreen}
+      options={{tabBarIcon:() => <MaterialCommunityIcons name="home-outline" size={24} color="white" /> }}/>
     </Tab.Navigator>
   );
 }
-
 
 const style = StyleSheet.create({
 
